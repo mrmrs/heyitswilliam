58c96d954534778a2687d12d941741df2a4d3fc2
mrmrs <hi@mrmrs.cc>
2013-03-13 18:34:45 -0700

    Adding current production source / assets for heyitswilliam.com

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Home | Hey It&#x27;s William</title>
+	<script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/scrollTo.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/effects.core.js" type="text/javascript" charset="utf-8"></script>
+	<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8" />
+	<!--[if IE]>
+		<link rel="stylesheet" href="css/styleie.css" type="text/css" media="screen" />
+	<![endif]-->
+</head>
+<body id="new_index">
+	<div id="pageTitle">
+		<h1>Hey It's William</h1>
+		<h2>The artwork of will deutsch</h2>
+		<h3>Los Angeles based artist and illustrator</h3>
+	</div>
+	<div id="nav">
+	<ul id="mainNav">
+		<li id="home"><a href="index.php" title="Home">Home</a></li>
+		<li id="blog"><a href="blog_redirect.php" title="Blog of Will Deutsch">Blog</a></li>
+		<li id="about"><a href="about.php" title="About">About</a></li>
+		<li id="order"><a href="order.php" title="Order Will Deutsch Art">Order</a></li>
+		<li id="contact"><a href="contact.php" title="Contact Will Deutsch">Contact</a></li>
+		<li id="links"><a href="links.php" title="Links">Links</a></li>
+	</ul>
+	<h2 id="portfolio">Portfolio</h2>
+	<ul id="portfolioNav">
+		<li><a href="paint.php" id="paintTab">Painting</a></li>
+		<li><a href="draw.php" id="drawTab">Drawing</a></li>
+		<li><a href="sculpt.php" id="sculptTab">Sculpting</a></li>
+	</ul>
+</div>
+<div id="content">
+	<div class="titleWrapper"><h2>Home</h2></div>
+	<div class="contentWrapper">
+		<p><span><strong>Welcome to the new heyitswilliam.com</strong> - We're still implementing many of the features across the site so please bear with us over the next few days.
+			</span> 
+		</p>
+	</div>
+</div>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-2373474-1";
+urchinTracker();
+</script>
+</body>
+</html>
