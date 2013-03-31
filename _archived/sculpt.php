1f509a0282db2d3602d137b5e6ef76a9864b356a
mrmrs <hi@mrmrs.cc>
2013-03-31 03:58:05 -0700

    Adding stuffs.

---
+++ _archived/sculpt.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Sculpture Portfolio | Hey It&#x27;s William</title>
+	<script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/effects.core.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/scrollTo.js" type="text/javascript" charset="utf-8"></script>
+	<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8" />
+		<!--[if IE]>
+		<link rel="stylesheet" href="css/styleie.css" type="text/css" media="screen" />
+	<![endif]-->
+</head>
+<body id="new_index">
+	<div id="pageTitle">
+		<h1>Hey It's William</h1>
+		<h2>The artwork of will deutsch</h2>
+		<h3>Los Angeles based artist and illustrator</h3>
+	</div>
+	<div id="nav">
+		<ul id="mainNav">
+			<li id="home"><a href="index.php" title="Home">Home</a></li>
+			<li id="blog"><a href="blog_redirect.php" title="Blog of Will Deutsch">Blog</a></li>
+			<li id="about"><a href="about.php" title="About">About</a></li>
+			<li id="order"><a href="order.php" title="Order Will Deutsch Art">Order</a></li>
+			<li id="contact"><a href="contact.php" title="Contact Will Deutsch">Contact</a></li>
+			<li id="links"><a href="links.php" title="Links">Links</a></li>
+		</ul>
+		<h2 id="portfolio">Portfolio</h2>
+			<ul id="portfolioNav">
+			<li><a href="paint.php" id="paintTab">Painting</a></li>
+			<li><a href="draw.php" id="drawTab">Drawing</a></li>
+			<li class="active"><a href="sculpt.php" id="sculptTab">Sculpting</a>
+				<ul id="sculptNav" class="selected">
+					<li><a href="#img1" title="title">#1</a></li>
+					<li><a href="#img2" title="title">#2</a></li>
+					<li><a href="#img3" title="title">#3</a></li>
+					<li><a href="#img4" title="title">#4</a></li>
+					<li><a href="#img5" title="title">#5</a></li>
+					<li><a href="#img6" title="title">#6</a></li>
+					<li><a href="#img7" title="title">#7</a></li>
+					<li><a href="#img8" title="title">#8</a></li>
+					<li><a href="#img9" title="title">#9</a></li>
+					<li><a href="#img10" title="title">#10</a></li>
+					<li><a href="#img11" title="title">#11</a></li>
+					<li><a href="#img12" title="title">#12</a></li>
+					<li><a href="#img13" title="title">#13</a></li>
+					<li><a href="#img14" title="title">#14</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	<div id="content">
+		<h2>Lightboxes</h2>
+      <p>
+        <strong>Altar to the Forlorn and Broken Hearted</strong> - This piece is comprised of 13 stained glass lightboxes. 6' x 7'.
+			</p>
+			<img src="portfolio/sculpture/lightbox_01.jpg" width="500" height="666" alt="Lightbox 01" id="img1"/>
+			<img src="portfolio/sculpture/lightbox_02.jpg" width="500" height="666" alt="Lightbox 02" id="img2"/>
+			<img src="portfolio/sculpture/lightbox_03.jpg" width="500" height="666" alt="Lightbox 03" id="img3"/>
+			<img src="portfolio/sculpture/lightbox_04.jpg" width="500" height="666" alt="Lightbox 04" id="img4"/>
+			<img src="portfolio/sculpture/lightbox_05.jpg" width="500" height="666" alt="Lightbox 05" id="img5"/>
+			<img src="portfolio/sculpture/lightbox_06.jpg" width="500" height="666" alt="Lightbox 06" id="img6"/>
+			<img src="portfolio/sculpture/lightbox_07.jpg" width="500" height="666" alt="Lightbox 07" id="img7"/>
+			<img src="portfolio/sculpture/lightbox_08.jpg" width="500" height="666" alt="Lightbox 08" id="img8"/>
+			<img src="portfolio/sculpture/lightbox_09.jpg" width="500" height="666" alt="Lightbox 09" id="img9"/>
+			<img src="portfolio/sculpture/lightbox_10.jpg" width="500" height="666" alt="Lightbox 10" id="img10"/>
+			<img src="portfolio/sculpture/lightbox_11.jpg" width="500" height="666" alt="Lightbox 11" id="img11"/>
+			<img src="portfolio/sculpture/lightbox_12.jpg" width="500" height="666" alt="Lightbox 12" id="img12"/>
+			<img src="portfolio/sculpture/lightbox_13.jpg" width="500" height="693" alt="Lightbox 13" id="img13"/>
+			<img src="portfolio/sculpture/lightbox_14.jpg" width="500" height="666" alt="Lightbox 14" id="img14"/>	
+		</div>
+	</div>
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-2373474-1";
+urchinTracker();
+</script>
+</body>
+</html>
+
+
+
---
+++ _site/js/menu.js
@@ -0,0 +1,6 @@
+	$(document).ready(function() {
+		$('.navPortfolio').hide();
+		$('#menu').click(function(){
+			$('.navPortfolio').toggle('slow');
+		});
+	});
